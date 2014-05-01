5e8b45718e4332bbd91816c591d52ac183b005e3
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-05-01 16:38:04 +0100

    Added example embedding IronPython using the ScriptEngine.

---
+++ IpyPlay/CSharpScriptEngine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using IronPython.Hosting;
+
+namespace CSharpScriptEngine
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Remember: this block code simply reads the function 
+            // definition contained in simple.py. It does not actually 
+            // execute the function defined by the script.
+            var engine = Python.CreateEngine();
+            var script = engine.CreateScriptSourceFromFile("simple.py");
+            var scope = engine.CreateScope();
+            script.Execute(scope);
+
+            // Now that I've defined the function by executing the script,
+            // I must actually invoke the function to see its effects.
+            var simpleFunc = scope.GetVariable("simple");
+            simpleFunc();
+
+            // Wait to read console.
+            Console.ReadLine();
+        }
+    }
+}
