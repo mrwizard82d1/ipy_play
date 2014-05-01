3b6705893665ca476ed246d27d8fa038b7c90362
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-05-01 19:52:51 +0100

    Added example of calling another assemby in project.

---
+++ IpyPlay/CSharpIpyAssy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using IronPython.Hosting;
+
+namespace CSharpIpyAssy
+{
+    class Program
+    {
+        static void Main()
+        {
+            var ipyRuntime = Python.CreateRuntime();
+            dynamic test = ipyRuntime.UseFile("work_task_test.py");
+            Console.WriteLine(test.test_abutting() ? 
+                              "They abut!" : 
+                              "No such luck.");
+            Console.WriteLine(test.test_not_abutting() ?
+                              "They abut!" :
+                              "No such luck.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ IpyPlay/WorkTasks/WorkTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WorkTasks
+{
+    /// <summary>
+    /// Models a task done at work.
+    /// </summary>
+    public class WorkTask
+    {
+        public DateTimeOffset Start { get; private set; }
+        public string Details { get; private set; }
+        public TimeSpan Duration { get; set; }
+
+        public WorkTask(DateTimeOffset start,
+                        string details)
+
+        {
+            Start = start;
+            Details = details;
+            Duration = TimeSpan.Zero;
+        }
+
+        /// <summary>
+        /// Do I abut anotherWorkTask?
+        /// </summary>
+        /// <param name="anotherWorkTask">
+        /// The <see cref="WorkTask"/> to test.
+        /// </param>
+        /// <returns>True if I abut anotherWorkTask; otherwise, false.</returns>
+        public bool IsAbutting(WorkTask anotherWorkTask)
+        {
+            return ((anotherWorkTask.Start + anotherWorkTask.Duration).Equals(Start) ||
+                    (Start + Duration).Equals(anotherWorkTask.Start));
+        }
+    }
+}
