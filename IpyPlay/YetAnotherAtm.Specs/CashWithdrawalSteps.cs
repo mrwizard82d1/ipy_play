b10bf78d88da0c4622591a524e4fb28c5bdfe1f5
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-05-02 17:17:21 +0100

    Moved all domain access into IronPython code.

--- IpyPlay/YetAnotherAtm.Specs/CashWithdrawalSteps.cs
+++ IpyPlay/YetAnotherAtm.Specs/CashWithdrawalSteps.cs
@@ -1,40 +1,23 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
-using IronPython.Hosting;
-using Microsoft.Scripting.Hosting;
-
-using Atm;
-
+﻿using IronPython.Hosting;
 using TechTalk.SpecFlow;
-using NUnit.Framework;
 
 namespace YetAnotherAtm.Specs
 {
     [Binding]
     public class CashWithdrawalSteps
     {
-        private Account _account;
-        private readonly string _accountNumber;
-
         private readonly dynamic _steps;
 
         public CashWithdrawalSteps()
         {
             var ipyRuntime = Python.CreateRuntime();
-            ipyRuntime.LoadAssembly(typeof(Account).Assembly);
-
             _steps = ipyRuntime.UseFile("cash_withdrawal_steps.py");
-
-            _accountNumber = "125009";
         }
 
         [Given(@"I have \$(.*) in my account")]
         public void GivenIHaveInMyAccount(decimal openingBalance)
         {
-            _account = _steps.create_account(_accountNumber,
-                                             openingBalance);
-            Assert.AreEqual(_accountNumber, _account.AccountNumber);
-            Assert.AreEqual(openingBalance, _account.Balance);
+            _steps.create_account(openingBalance);
         }
 
         [When(@"I request \$(.*)")]
--- IpyPlay/YetAnotherAtm.Specs/cash_withdrawal_steps.py
+++ IpyPlay/YetAnotherAtm.Specs/cash_withdrawal_steps.py
@@ -1,11 +1,17 @@
-# import clr
-# clr.AddReferenceToFileAndPath('Atm.dll')
-import Atm
+import clr
+clr.AddReferenceToFileAndPath('Atm.dll')
+from Atm import Account
 
 __author__ = 'l.jones'
 
 
-def create_account(account_number, opening_balance):
-    """Create an account from account_number and opening_balance."""
-    return Atm.Account(account_number, opening_balance)
+account_number = '125009'
+cut = None
+
+
+def create_account(opening_balance):
+    """Create an account with opening_balance (in this module)."""
+    global cut
+    cut = Account(account_number, opening_balance)
+    
 
